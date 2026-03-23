6180e2aa21f040356229879b22b1792edc83f91f
WInner Michael <winnermichael2010@gmail.com>
2026-03-23 06:27:06 -0700

    add everything

--- ques2.js
+++ ques2.js
@@ -1,1 +1,9 @@
-function 
+function isEven(number){
+    let sum=0
+    if (number % 2=== 0){sum=`true`} else if(number % 2=== 1){sum=`false`}
+    
+
+    console.log(sum)
+    return sum
+}
+isEven(40)
